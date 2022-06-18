f82d6cd25bf356e35be0bc84790da633ca299e52
ftnext <takuyafjp+develop@gmail.com>
2022-06-18 11:21:53 +0900

    [feat] Lower case transformation

--- refactoring/rot13.py
+++ refactoring/rot13.py
@@ -1,2 +1,24 @@
-def transform(input_):
-    return ""
+def transform(input_: str) -> str:
+    result = ""
+    for character in input_:
+        char_code = ord(character)
+        result += transform_letter(char_code)
+    return result
+
+
+def transform_letter(char_code: int) -> str:
+    if is_between(char_code, "a", "m"):
+        char_code += 13
+    elif is_between(char_code, "n", "z"):
+        char_code -= 13
+    return chr(char_code)
+
+
+def is_between(char_code: int, first_letter: str, last_letter: str) -> bool:
+    return char_code >= code_for(first_letter) and char_code <= code_for(
+        last_letter
+    )
+
+
+def code_for(letter: str) -> int:
+    return ord(letter)
--- refactoring/test_rot13.py
+++ refactoring/test_rot13.py
@@ -6,3 +6,9 @@ import rot13
 class Rot13TestCase(TestCase):
     def test_does_nothing_when_input_is_empty(self):
         self.assertEqual(rot13.transform(""), "")
+
+    def test_transforms_lower_case_letters(self):
+        self.assertEqual(
+            rot13.transform("abcdefghijklmnopqrstuvwxyz"),
+            "nopqrstuvwxyzabcdefghijklm",
+        )
